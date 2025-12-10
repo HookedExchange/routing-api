0af87d73024e393b8ffc0b32d6edcc44ea45980a
Tachy <->
2025-12-10 15:00:54 +0200

    feat: overwrite uni rpc for ETH

--- lib/rpc/utils.ts
+++ lib/rpc/utils.ts
@@ -37,9 +37,15 @@ export function chainIdToNetworkName(networkId: ChainId): string {
 
 export function generateProviderUrl(key: string, value: string, chainId: number): string {
   if (key === 'UNIRPC_0') {
-    // UNIRPC_0 is a special case for the Uniswap RPC
-    // - env value will contain the generic unirpc endpoint - no trailing '/'
-    return `${value}/rpc/${chainId}`
+    switch (chainId) {
+      // skip uniRPC and just return what was given
+      case 1:
+        return value
+      // UNIRPC_0 is a special case for the Uniswap RPC
+      // - env value will contain the generic unirpc endpoint - no trailing '/'
+      default:
+        return `${value}/rpc/${chainId}`
+    }
   }
 
   const tokens = value.split(',')
